f284db02cf9a6937de5099c4b024dbb0225f6f89
Lily <aulily09@gmail.com>
2023-07-27 15:33:52 -0700

    Implement getUser axios call

--- src/App.js
+++ src/App.js
@@ -29,10 +29,24 @@ function App() {
       });
   }
 
+  const getUser = () => {
+    // Test Data, eventually user data will be passed down
+    const userId = "64c2e9cc8d528bb29bc102b8"
+    axios.get(`${API_URL}/get-users/${userId}`)
+      .then((response) => {
+        console.log("This is the user info", response)
+      })
+      .catch((error) => {
+        console.log("error: ", error);
+      })
+  }
+
+
   return (
     <div>
       <h1>Restaurant Recommendation >: D</h1>
       <button onClick={createUser}>Create User</button>
+      <button onClick={getUser}>Get User</button>
       <UserForm createUser={createUser} />
       <Map />
     </div>
