77f171cdb7d726e5307a4e020bb49b2daa2ce8aa
Lily <aulily09@gmail.com>
2023-07-27 19:23:47 -0700

    Remove extra / on API_URL

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import UserForm from "./UserForm";
 // const sampleData = []
 
 
-const API_URL = "https://restaurant-rec-api-back-end.onrender.com/record/"
+const API_URL = "https://restaurant-rec-api-back-end.onrender.com/record"
 
 function App() {
 
