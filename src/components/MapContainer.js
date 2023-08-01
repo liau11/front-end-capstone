b9950f9994a0dc0b66d0efbc2ce2152e373e0816
Lily <aulily09@gmail.com>
2023-08-01 15:39:03 -0700

    Add custom marker

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -1,22 +1,34 @@
 import React from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import L from "leaflet";
+
 
 import "leaflet/dist/leaflet.css";
 
-const Map = ({recommendationsData}) => {
+const Map = ({ recommendationsData }) => {
     const mapStyle = { width: "100%", height: "500px" };
 
     const center = [47.6206, -122.3505];
     const zoom = 13;
 
+    var customMarker = L.icon({
+        iconUrl: "https://www.freepnglogos.com/uploads/pin-png/pin-transparent-png-pictures-icons-and-png-backgrounds-30.png",
+        iconSize: [30, 30],
+        popupAnchor: [-3, -20]
+    });
+
     const markers = recommendationsData.map((restaurant) => {
         return (
-        <Marker position={[restaurant.coordinates.latitude, restaurant.coordinates.longitude]}>
-            <Popup>
-                <h2>{restaurant.name}</h2>
-            </Popup>
-        </Marker>
-        )}
+            <Marker
+                position={[restaurant.coordinates.latitude, restaurant.coordinates.longitude]}
+                icon={customMarker}
+            >
+                <Popup>
+                    <h2>{restaurant.name}</h2>
+                </Popup>
+            </Marker>
+        )
+    }
     );
 
     return (
