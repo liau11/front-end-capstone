46717a80d15700c68fa3c8fcff38bab506aaafd7
Sophia Tran <sophiat8832@gmail.com>
2023-08-02 20:47:21 -0700

    Create HomePage and NotFoundPage

---
+++ src/pages/HomePage.js
@@ -0,0 +1,10 @@
+const HomePage = () => {
+    return (
+        <section>
+            <h1>Welcome to the Home Page!</h1>
+        </section>
+    );
+
+};
+
+export default HomePage;
---
+++ src/pages/NotFoundPage.js
@@ -0,0 +1,10 @@
+const NotFoundPage = () => {
+    return (
+        <section>
+            <h1>How'd You Get Here?</h1>
+            <p>It looks like this doesn't exist... Sorry!</p>
+        </section>
+    )
+};
+
+export default NotFoundPage;
