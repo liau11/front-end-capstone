db0edabc447a1f9b664b22b2b7a662d5680b3705
Sophia Tran <sophiat8832@gmail.com>
2023-08-02 20:53:56 -0700

    Added navigation bar with links

--- src/Routes.js
+++ src/Routes.js
@@ -1,9 +1,17 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';import HomePage from "./pages/HomePage";
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';import HomePage from "./pages/HomePage";
 import NotFoundPage from "./pages/NotFoundPage";
 
 const RoutesPath = () => {
     return (
         <Router>
+            <nav style={{ margin: 10 }}>
+                <Link to="/" style={{ padding: 5 }}>
+                Home
+                </Link>
+                <Link to="/not-found" style={{ padding: 5 }}>
+                About
+                </Link>
+            </nav>
             <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route path="/not-found" element={<NotFoundPage />} />
