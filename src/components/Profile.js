e0a830185f93555acad5631b297f64b4bdeed17a
Lily <aulily09@gmail.com>
2023-08-03 14:15:32 -0700

    Current progress, user not authenticated

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -18,32 +18,35 @@ import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
 const Profile = () => {
-    const { user, isLoading, isAuthenticated } = useAuth0();
+    const { user, isAuthenticated, isLoading } = useAuth0();
 
     // Check if authentication is still loading
-    if (isLoading) {
-        return <div>Loading ...</div>;
-    }
+    // if (isLoading) {
+    //     return <div>Loading ...</div>;
+    // }
 
     // Check if user is authenticated
-    if (!isAuthenticated) {
-        return <div>Not authenticated</div>;
-    }
+    // if (!isAuthenticated) {
+    //     console.log(user);
+    //     < div > Not authenticated</div >;
+    // }
+
 
     // Check if user object is defined
-    if (!user) {
-        return <div>User data not available</div>;
-    }
+    // if (!user) {
+    //     return <div>User data not available</div>;
+    // }
 
     console.log(user);
 
     return (
-        <div>
-            {/* Display user information */}
-            <h2>Email: {user.email}</h2>
-            {/* You can also display other user information */}
-            <pre>{JSON.stringify(user, null, 2)}</pre>
-        </div>
+        isAuthenticated && (
+            <div>
+                {/* <img src={user.picture} alt={user.name} /> */}
+                <h2>{user.name}</h2>
+                <p>{user.email}</p>
+            </div>
+        )
     );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,22 +1,29 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
+// import { createRoot } from 'react-dom/client';
 import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App';
 
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const myDomain = process.env.REACT_APP_AUTH0_DOMAIN;
-const myClientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
-
+const myDomain = `${process.env.REACT_APP_AUTH0_DOMAIN}`;
+const myClientId = `${process.env.REACT_APP_AUTH0_CLIENT_ID}`;
 
 root.render(
-  < Auth0Provider
-    domain={myDomain}
-    clientId={myClientId}
-    authorizationParams={{
-      redirect_uri: window.location.origin
-    }}
-  >
-    <App />
-  </Auth0Provider >,
+  <div>
+    < Auth0Provider
+      domain={myDomain}
+      clientId={myClientId}
+      authorizationParams={{
+        redirect_uri: window.location.origin
+      }}
+    >
+      <App />
+    </Auth0Provider >
+  </div>
 );
+
+
+
