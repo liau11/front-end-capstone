086c807091b58d285b7f544263b1b30e39fee457
Sophia Tran <sophiat8832@gmail.com>
2023-08-02 23:32:22 -0700

    Add Routes component with useRoutes

--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,43 @@
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';import HomePage from "./pages/HomePage";
+import { BrowserRouter as Router, useRoutes, Link, Outlet, useParams } from 'react-router-dom';
+import HomePage from "./pages/HomePage";
 import NotFoundPage from "./pages/NotFoundPage";
+// import RecommendationsResultsList from './components/RecommendationsResultsList';
+
+const slug = "something";
+
+function Testing() {
+    console.log("I AM IN TESTING COMPONENT")
+    // const { slug } = useParams();
+    return (
+        <h2>I AM TESTING</h2>
+    );
+}
+
+function List() {
+    return (
+        <Link to='/forms/something'>
+            <h2>Get Lists</h2>
+        </Link>
+    );
+}
+
+function Routes() {
+    const element = useRoutes([
+    { path: "/", element: <HomePage/> },
+    { path: "/testing", element: <Testing />},
+    { 
+        path: "/forms",
+        element: <List/>,
+        children: [
+        // { index: true, element: <List/> },
+        { path: "something", element: <Testing/> }
+        ],
+    },
+    // { path: "/not-found", element: <NotFoundPage/> },
+    { path: "*", element: <NotFoundPage/>}
+    ]);
+    return element;
+}
 
 const RoutesPath = () => {
     return (
@@ -8,14 +46,17 @@ const RoutesPath = () => {
                 <Link to="/" style={{ padding: 5 }}>
                 Home
                 </Link>
+                <Link to="/forms" style={{ padding: 5 }}>
+                Forms
+                </Link>
+                <Link to="/testing" style={{ padding: 5 }}>
+                Testing
+                </Link>
                 <Link to="/not-found" style={{ padding: 5 }}>
-                About
+                Not Found
                 </Link>
             </nav>
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/not-found" element={<NotFoundPage />} />
-            </Routes>
+            <Routes />
         </Router>
     );
 };
