e6d4b0848c7f0dbb6716b7d648ec56fe397a0438
Lily <aulily09@gmail.com>
2023-08-04 10:07:23 -0700

    Current progress - has bug

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,47 +1,19 @@
-// import React from 'react';
-// import { useAuth0 } from '@auth0/auth0-react';
-
-// const Profile = () => {
-
-//     const { user } = useAuth0();
-
-//     return (
-//         <div>
-//             {JSON.stringify(user, null, 2)}
-//         </div>
-//     )
-// }
-
-// export default Profile
-
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
 const Profile = () => {
     const { user, isAuthenticated, isLoading } = useAuth0();
 
-    // Check if authentication is still loading
-    // if (isLoading) {
-    //     return <div>Loading ...</div>;
-    // }
-
-    // Check if user is authenticated
-    // if (!isAuthenticated) {
-    //     console.log(user);
-    //     < div > Not authenticated</div >;
-    // }
-
-
-    // Check if user object is defined
-    // if (!user) {
-    //     return <div>User data not available</div>;
-    // }
+    if (isLoading) {
+        return <div>Loading ...</div>;
+    }
 
     console.log(user);
 
     return (
         isAuthenticated && (
             <div>
+                {user}
                 {/* <img src={user.picture} alt={user.name} /> */}
                 <h2>{user.name}</h2>
                 <p>{user.email}</p>
