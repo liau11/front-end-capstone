320a54bb97c55e1b3609834571d6c9dd8faa2c0f
Sophia Tran <sophiat8832@gmail.com>
2023-08-06 16:52:06 -0700

    Uncomment allResults to render each friend data

--- src/pages/FriendsPage.js
+++ src/pages/FriendsPage.js
@@ -5,19 +5,19 @@ const FriendsPage = ({ users, updateUserAdd, currentFriends }) => {
 
     console.log("I'm in Friends Page. Users:", users)
 
-    // const allResults = currentFriends.map((friend) => {
-    //     return (<FriendData
-    //         key={friend._id}
-    //         friendId={friend._id}
-    //         name={friend.name}
-    //     />
-    //     )
-    // });
+    const allResults = currentFriends.map((friend) => {
+        return (<FriendData
+            key={friend._id}
+            friendId={friend._id}
+            name={friend.name}
+        />
+        )
+    });
 
     return (
         <section>
             <FindFriendForm users={users} updateUserAdd={updateUserAdd} />
-            {/* {allResults} */}
+            {allResults}
         </section>
     )
 };
