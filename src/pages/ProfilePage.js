cfc184773688b407165a68cc598026e8a7b45302
Sophia Tran <sophiat8832@gmail.com>
2023-08-07 15:11:41 -0700

    Create ProfilePage

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,13 +1,24 @@
 import FriendsPage from "./FriendsPage";
+import UserRecommendationData from "../components/UserRecommendationData";
 
-const ProfilePage = ({ currentUser, users, updateUserAdd, currentFriends }) => {
+const ProfilePage = ({ getUserRecommendations, users, updateUserAdd, currentFriends }) => {
 
+    // const allResults = currentUser.recommendations.map((restaurant) => {
+    //     return (<UserRecommendationData
+    //         key={friend._id}
+    //         friendId={friend._id}
+    //         name={friend.name}
+    //     />
+    //     )
+    // });
 
     return (
         <section>
-            <FriendsPage currentUser={currentUser} users={users} updateUserAdd={updateUserAdd} currentFriends={currentFriends}/>
+            {/* <FriendsPage currentUser={currentUser} users={users} updateUserAdd={updateUserAdd} currentFriends={currentFriends}/>
+            {allResults} */}
         </section>
     )
 };
 
 export default ProfilePage;
+
