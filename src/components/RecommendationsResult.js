a07e5e6abdba8404466d3620862b7915398f7b76
Sophia Tran <sophiat8832@gmail.com>
2023-08-10 11:31:52 -0700

    Add Card component styling

--- src/components/RecommendationsResult.js
+++ src/components/RecommendationsResult.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './RecommendationsResult.css';
-
+import Card from 'react-bootstrap/Card';
 
 const RecommendationsResult = (props) => {
 
@@ -17,7 +17,25 @@ const RecommendationsResult = (props) => {
 
     return (
         <section>
-            <li>
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={props.imageUrl}/>
+                <Card.Body>
+                    <Card.Title>{props.name}</Card.Title>
+                    <Card.Text>
+                        <div>{addressString}</div>
+                        <div>{props.display_phone}</div>
+                        <div>{props.price}</div>
+                    </Card.Text>
+                </Card.Body>
+                <Card.Body>
+                    <button onClick={openYelpInNewTab}>Yelp</button>
+                    <button onClick={() => props.handleAddToList("savedList", {"savedList":props.restaurantId } )}>Save for Later</button>
+                    <button onClick={() => props.handleAddToList("recommendations", {"recommendations": props.restaurantId})}>Upvote, also recommend!</button>
+                    <Card.Link href="#">Card Link</Card.Link>
+                    <Card.Link href="#">Another Link</Card.Link>
+                </Card.Body>
+            </Card>
+            {/* <li>
                 <img src={props.imageUrl} alt={props.name} /> 
                 <h2>{props.name}</h2>
                 <div>{addressString}</div>
@@ -26,7 +44,7 @@ const RecommendationsResult = (props) => {
             </li>
             <button onClick={openYelpInNewTab}>Yelp</button>
             <button onClick={() => props.handleAddToList("savedList", {"savedList":props.restaurantId } )}>Save for Later</button>
-                <button onClick={() => props.handleAddToList("recommendations", {"recommendations": props.restaurantId})}>Upvote, also recommend!</button>
+            <button onClick={() => props.handleAddToList("recommendations", {"recommendations": props.restaurantId})}>Upvote, also recommend!</button> */}
         </section>
     );
 };
