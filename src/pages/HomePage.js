e5579a402b0e824d5bf419d35fd0f7d352f3e639
Sophia Tran <sophiat8832@gmail.com>
2023-08-10 11:32:22 -0700

    Use rows and cols to display recommendation result list

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,25 +2,43 @@ import Map from "../components/MapContainer";
 import RecommendationsResultsList from "../components/RecommendationsResultsList";
 import GetRecommendationsForm from "../components/GetRecommendationsForm";
 import './HomePage.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const HomePage = ({ handleAddToList, currentUser, recommendationsData, updateUserAdd, getFriendsRecommendations, cityCenter }) => {
     return (
         <section>
             <h1>Welcome to the Home Page!</h1>
-            < Map 
-                cityCenter={cityCenter} 
-                recommendationsData={recommendationsData} 
-            />
-            <RecommendationsResultsList 
-                handleAddToList={handleAddToList} 
-                currentUser={currentUser} 
-                recommendationsData={recommendationsData} 
-                updateUserAdd={updateUserAdd} 
-            />
-            <GetRecommendationsForm  
-                currentUser={currentUser} 
-                getFriendsRecommendations={getFriendsRecommendations}
-            />
+            <Container fluid>
+                <Row>
+                    <Col sm={5} >
+                        < Map 
+                            cityCenter={cityCenter} 
+                            recommendationsData={recommendationsData} 
+                        />
+                    </Col>
+                    <Col>
+                        <RecommendationsResultsList 
+                            handleAddToList={handleAddToList} 
+                            currentUser={currentUser} 
+                            recommendationsData={recommendationsData} 
+                            updateUserAdd={updateUserAdd} 
+                        />
+                    </Col>
+                </Row>
+                <Row>
+                    {/* <Col>1 of 3</Col>
+                    <Col>2 of 3</Col>
+                    <Col>3 of 3</Col> */}
+                    <GetRecommendationsForm  
+                        currentUser={currentUser} 
+                        getFriendsRecommendations={getFriendsRecommendations}
+                    />
+                </Row>
+            </Container>
+    
+            
         </section>
     );
 
