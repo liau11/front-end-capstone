e12a54b67be2b7101ebcb8530e59577b45e9c51c
liau11 <aulily09@gmail.com>
2023-08-10 19:19:34 -0700

    Change to single quotations for consistency

--- src/components/UserRecommendationData.js
+++ src/components/UserRecommendationData.js
@@ -10,16 +10,16 @@ const UserRecommendationData = ({ updateUserDelete, restaurantId, name, address,
     };
 
     return (
-        <Card style={{ width: '18rem' }} className="border p-4">
-            <div className="card-image-container">
-                <Card.Img variant="top" src={imageUrl} className="centered-image" />
+        <Card style={{ width: '18rem', marginTop: '30px'}} className='border p-4'>
+            <div className='card-image-container'>
+                <Card.Img variant='top' src={imageUrl} className='centered-image' />
             </div>
             <Card.Body>
                 <Card.Title>{name}</Card.Title>
                 <Card.Text>{address}</Card.Text>
-								<Card.Footer  className="d-flex justify-content-center align-items-center">
-                <Button variant="primary" onClick={openYelpInNewTab}>Yelp</Button>
-                <Button variant="danger" onClick={() => updateUserDelete('recommendations', {'recommendations': restaurantId })}>
+								<Card.Footer  className='d-flex justify-content-center align-items-center'>
+                <Button variant='primary' onClick={openYelpInNewTab}>Yelp</Button>
+                <Button variant='danger' onClick={() => updateUserDelete('recommendations', {'recommendations': restaurantId })}>
                     Remove Recommendation 
                 </Button>
 								</Card.Footer>
--- src/components/UserSavedData.js
+++ src/components/UserSavedData.js
@@ -9,16 +9,16 @@ const UserSavedData = ({  restaurantId, name, address, updateUserDelete, url, im
     };
 
     return (
-      <Card style={{ width: '18rem' }} className="border p-4">
-          <div className="card-image-container">
-              <Card.Img variant="top" src={imageUrl} className="centered-image" />
+      <Card style={{ width: '18rem', marginTop: '40px' }} className='border p-4'>
+          <div className='card-image-container'>
+              <Card.Img variant='top' src={imageUrl} className='centered-image' />
           </div>
           <Card.Body>
             <Card.Title>{name}</Card.Title>
             <Card.Text>{address}</Card.Text>
-                <Card.Footer  className="d-flex justify-content-center align-items-center">
-                <Button variant="primary" onClick={openYelpInNewTab}>Yelp</Button>
-                <Button variant="danger" onClick={() => updateUserDelete('savedList', {'savedList': restaurantId })}>
+                <Card.Footer  className='d-flex justify-content-center align-items-center'>
+                <Button variant='primary' onClick={openYelpInNewTab}>Yelp</Button>
+                <Button variant='danger' onClick={() => updateUserDelete('savedList', {'savedList': restaurantId })}>
                     Remove Recommendation 
                 </Button>
 				      </Card.Footer>
