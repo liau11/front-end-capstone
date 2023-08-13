1521a8cf2153e40e82f213a2b0b33840d0844033
liau11 <aulily09@gmail.com>
2023-08-13 11:51:24 -0700

    Reorg paragraphs

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -9,19 +9,20 @@ const AboutPage = () => {
 							dilemma when it comes to picking where to eat. They want to avoid wasting
 							time and money on dishes they're not crazy about. Although Yelp and Google
 							offer a useful starting point, relying solely on the opinions of
-							strangers doesn't always make it easier.
+							strangers doesn't always make it easier. Sophia and Lily believe that the
+							best recommendations come from friends, not total strangers. That's
+							 why they built this site. 
 						</p>
 
-					<p>Sophia and Lily believe that the best recommendations come from friends,
-							not total strangers. That's why they built this site. Want to tap into
-							the foodie wisdom of your pals? Start by adding them to your friend list.
-							Once you've got even just one food-loving buddy in your network, you're
-							in for a treat. You'll be able to peek at all the restaurants they love,
-							right in the city you're interested in. You'll see all those spots on
-							a map too, in case you're all about convenience. When you stumble upon
-							a restaurant that catches your eye, you've got options. You can save
-							it for later – think of it as a bookmark for your belly. Or, better yet,
-							share the love if you approve of the restaurant and add it to your own
+					<p>Want to tap into the foodie wisdom of your pals? Start by adding 
+							them to your friend list. Once you've got even just one food-loving
+						 	buddy in your network, you're in for a treat. You'll be able to peek
+						  at all the restaurants they love, right in the city you're interested
+							in. You'll see all those spots on a map too, in case you're all 
+							about convenience. When you stumble upon a restaurant that catches 
+							your eye, you've got options. You can save it for later – think of
+							it as a bookmark for your belly. Or, better yet, share the love
+							if you approve of the restaurant and add it to your own
 							list of recommendations. You can also access your profile page,
 							which you can revise should your preferences evolve. Tweak it
 							any time you want to update your flavor faves.
