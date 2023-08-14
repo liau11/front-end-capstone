9bf6aba313ff2452dc742badcbe1e043a4b98e64
liau11 <aulily09@gmail.com>
2023-08-14 13:19:33 -0700

    Style Google map button

--- src/components/RecommendationsResult.js
+++ src/components/RecommendationsResult.js
@@ -40,9 +40,11 @@ const RecommendationsResult = (props) => {
 						<Icon icon="bi:yelp" color="red" width="18" height="18" />
 						Yelp
 					</button>
-					<button class="btn btn-sm btn-google-maps" onClick={openGoogleMaps}>
-						<Icon icon="bi:map" color="purple" width="17" height="17"/> 
-						Google Maps
+					<button class="btn btn-sm btn-outline-dark" onClick={openGoogleMaps}>
+						<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-map" viewBox="0 0 16 16">
+  							<path fill-rule="evenodd" d="M15.817.113A.5.5 0 0 1 16 .5v14a.5.5 0 0 1-.402.49l-5 1a.502.502 0 0 1-.196 0L5.5 15.01l-4.902.98A.5.5 0 0 1 0 15.5v-14a.5.5 0 0 1 .402-.49l5-1a.5.5 0 0 1 .196 0L10.5.99l4.902-.98a.5.5 0 0 1 .415.103zM10 1.91l-4-.8v12.98l4 .8V1.91zm1 12.98 4-.8V1.11l-4 .8v12.98zm-6-.8V1.11l-4 .8v12.98l4-.8z"/>
+						</svg>						
+					Google Maps
 					</button>
 					<button class="btn btn-outline-primary btn-sm" onClick={() => props.handleAddToList("savedList", {"savedList":props.restaurantId } )}>
 						<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-bookmark-heart" viewBox="0 0 16 16">
