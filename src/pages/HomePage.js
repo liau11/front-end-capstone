779cc830bd948773b4f97925e9dd0b2f18568af1
Sophia Tran <sophiat8832@gmail.com>
2023-08-14 14:41:30 -0700

    Place both forms into Card with Tabs

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,17 +3,58 @@ import RecommendationsResultsList from "../components/RecommendationsResultsList
 import GetRecommendationsForm from "../components/GetRecommendationsForm";
 import RestaurantForm from "../components/RestaurantForm";
 import './HomePage.css';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import { Container, Row, Col, Tab, Tabs, Card } from 'react-bootstrap';
 
 const HomePage = ({ handleAddToList, currentUser, recommendationsData, updateUserAdd, getFriendsRecommendations, cityCenter, allRestaurants, addNewRestaurant }) => {
     return (
         <section className="body">
             {/* <h2 className="text-center"> Welcome to the Home Page! </h2> */}
             <br />
+            <Container className="forms-container">
+                <Card>
+                    <Card.Header>
+                        <Tabs
+                        defaultActiveKey="find"
+                        id="justify-tab-example"
+                        className="mb-3"
+                        justify
+                        >
+                            <Tab eventKey="find" title="Find Restaurants">
+                                <GetRecommendationsForm 
+                                currentUser={currentUser} 
+                                getFriendsRecommendations={getFriendsRecommendations}
+                                />
+                            </Tab>
+                            <Tab eventKey="recommend" title="Recommend a Restaurant">
+                                <RestaurantForm
+                                currentUser={currentUser}
+                                handleAddToList={handleAddToList} 
+                                allRestaurants={allRestaurants} 
+                                addNewRestaurant={addNewRestaurant} 
+                                updateUserAdd={updateUserAdd}
+                                />
+                            </Tab>
+                        </Tabs>
+                        {/* <Nav variant="pills" defaultActiveKey="#first">
+                            <Nav.Item>
+                                <Nav.Link href="#first">Search Restaurants</Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Nav.Link href="#link">Recommend Restaurant</Nav.Link>
+                            </Nav.Item>
+                        </Nav> */}
+                    </Card.Header>
+                    {/* <Card.Body>
+                        <Card.Title>Special title treatment</Card.Title>
+                        <Card.Text>
+                            With supporting text below as a natural lead-in to additional content.
+                        </Card.Text>
+                    </Card.Body> */}
+                </Card>
+                <br />
+            </Container>
             <Container fluid >
-                <Row className="text-center">
+                {/* <Row className="text-center">
                     <Col>
                         <GetRecommendationsForm 
                             currentUser={currentUser} 
@@ -29,7 +70,7 @@ const HomePage = ({ handleAddToList, currentUser, recommendationsData, updateUse
                             updateUserAdd={updateUserAdd}
                         ></RestaurantForm>
                     </Col>
-                </Row>
+                </Row> */}
                 <br />
                 <Row className="justify-content-center">
                     <Col md={8} lg={8} xl={8}>
