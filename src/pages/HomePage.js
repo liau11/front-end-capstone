78b6783b4b57d4c31d6376952b468f8a83053074
Sophia Tran <sophiat8832@gmail.com>
2023-08-15 11:39:27 -0700

    Make selected tab more prominent and delete commented code

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -8,15 +8,16 @@ import { Container, Row, Col, Tab, Tabs, Card } from 'react-bootstrap';
 const HomePage = ({ handleAddToList, currentUser, recommendationsData, updateUserAdd, getFriendsRecommendations, cityCenter, allRestaurants, addNewRestaurant }) => {
     return (
         <section className="body">
-            {/* <h2 className="text-center"> Welcome to the Home Page! </h2> */}
             <br />
             <Container className="forms-container">
                 <Card>
                     <Card.Header>
                         <Tabs
+                        // activeKey={activeTab} 
+                        // onSelect={handleTabSelect}
                         defaultActiveKey="find"
-                        id="justify-tab-example"
-                        className="mb-3"
+                        id="justify-tab"
+                        className="home-tabs mb-3"
                         justify
                         >
                             <Tab eventKey="find" title="Find Restaurants">
@@ -35,42 +36,11 @@ const HomePage = ({ handleAddToList, currentUser, recommendationsData, updateUse
                                 />
                             </Tab>
                         </Tabs>
-                        {/* <Nav variant="pills" defaultActiveKey="#first">
-                            <Nav.Item>
-                                <Nav.Link href="#first">Search Restaurants</Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item>
-                                <Nav.Link href="#link">Recommend Restaurant</Nav.Link>
-                            </Nav.Item>
-                        </Nav> */}
                     </Card.Header>
-                    {/* <Card.Body>
-                        <Card.Title>Special title treatment</Card.Title>
-                        <Card.Text>
-                            With supporting text below as a natural lead-in to additional content.
-                        </Card.Text>
-                    </Card.Body> */}
                 </Card>
                 <br />
             </Container>
             <Container fluid >
-                {/* <Row className="text-center">
-                    <Col>
-                        <GetRecommendationsForm 
-                            currentUser={currentUser} 
-                            getFriendsRecommendations={getFriendsRecommendations}
-                        />
-                    </Col>
-                    <Col>
-                        <RestaurantForm
-                            currentUser={currentUser}
-                            handleAddToList={handleAddToList} 
-                            allRestaurants={allRestaurants} 
-                            addNewRestaurant={addNewRestaurant} 
-                            updateUserAdd={updateUserAdd}
-                        ></RestaurantForm>
-                    </Col>
-                </Row> */}
                 <br />
                 <Row className="justify-content-center">
                     <Col md={8} lg={8} xl={8}>
