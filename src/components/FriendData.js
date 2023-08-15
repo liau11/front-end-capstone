f16aaaada0bc78c1a0b9e7e7d9d7615dc003305e
liau11 <aulily09@gmail.com>
2023-08-14 18:50:45 -0700

    Add Tooltip

--- src/components/FriendData.js
+++ src/components/FriendData.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import './FriendData.css';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Tooltip from 'react-bootstrap/Tooltip';
 
 const FriendData = (props) => {
 	return (
@@ -11,25 +13,28 @@ const FriendData = (props) => {
 					<Card.Title className="friend-name">{props.name}</Card.Title>
 					<Card.Text className="friend-email">Email: {props.email}</Card.Text>
 				</div>
-				<Button
-					variant="danger"
-					onClick={() => props.updateUserDelete("friends", {"friends": props.friendId})}
-				>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					width="16"
-					height="16"
-					fill="currentColor"
-					className="bi bi-person-x icon"
-					viewBox="0 0 16 16"
-				>
-					<path d="M11 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4Zm.256 7a4.474 4.474 0 0 1-.229-1.004H3c.001-.246.154-.986.832-1.664C4.484 10.68 5.711 10 8 10c.26 0 .507.009.74.025.226-.341.496-.65.804-.918C9.077 9.038 8.564 9 8 9c-5 0-6 3-6 4s1 1 1 1h5.256Z"/>
-					<path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7Zm-.646-4.854.646.647.646-.647a.5.5 0 0 1 .708.708l-.647.646.647.646a.5.5 0 0 1-.708.708l-.646-.647-.646.647a.5.5 0 0 1-.708-.708l.647-.646-.647-.646a.5.5 0 0 1 .708-.708Z"/>
-				</svg>
-			</Button>
+					<OverlayTrigger placement='top' overlay={<Tooltip id={'yelp-msg'} style={{position:"fixed"}}>Unfriend</Tooltip>}>
+						<Button
+							variant="danger"
+							onClick={() => props.updateUserDelete("friends", {"friends": props.friendId})}
+						>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="16"
+							height="16"
+							fill="currentColor"
+							className="bi bi-person-x icon"
+							viewBox="0 0 16 16"
+						>
+							<path d="M11 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4Zm.256 7a4.474 4.474 0 0 1-.229-1.004H3c.001-.246.154-.986.832-1.664C4.484 10.68 5.711 10 8 10c.26 0 .507.009.74.025.226-.341.496-.65.804-.918C9.077 9.038 8.564 9 8 9c-5 0-6 3-6 4s1 1 1 1h5.256Z"/>
+							<path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7Zm-.646-4.854.646.647.646-.647a.5.5 0 0 1 .708.708l-.647.646.647.646a.5.5 0 0 1-.708.708l-.646-.647-.646.647a.5.5 0 0 1-.708-.708l.647-.646-.647-.646a.5.5 0 0 1 .708-.708Z"/>
+						</svg>
+					</Button>
+				</OverlayTrigger>
 			</Card.Body>
 		</Card>
 	);
 };
 
 export default FriendData;
+
