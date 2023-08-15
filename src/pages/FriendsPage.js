aa53914388c00382c40416ea6755e5f4a935cbaa
liau11 <aulily09@gmail.com>
2023-08-14 19:21:11 -0700

    Make popup form and make button

--- src/pages/FriendsPage.js
+++ src/pages/FriendsPage.js
@@ -1,6 +1,11 @@
 import FindFriendForm from "../components/FindFriendForm";
 import FriendData from "../components/FriendData";
 import './FriendsPage.css';
+import Modal from 'react-bootstrap/Modal';
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+
+
 
 const FriendsPage = ({ updateUserDelete, setUpdated, currentUser, users, updateUserAdd, currentFriends }) => {
 
@@ -15,14 +20,36 @@ const FriendsPage = ({ updateUserDelete, setUpdated, currentUser, users, updateU
 			)
     });
 
-    return (
-			<section>
-				<div className="page-body">
-					<FindFriendForm setUpdated={setUpdated} currentUser={currentUser} users={users} updateUserAdd={updateUserAdd} />
-					{allResults}
+		const [show, setShow] = useState(false);
+
+		const handleClose = () => setShow(false);
+		const handleShow = () => setShow(true);
+
+	return (
+		<section>
+			<div className="page-body">
+				<div className="btn-add">
+					<Button variant="primary" onClick={handleShow}>
+						Add a friend
+					</Button>
 				</div>
-			</section>
-    )
+			<Modal show={show} onHide={handleClose}>
+				<Modal.Header closeButton>
+					<Modal.Title>Search for your friend</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					<FindFriendForm setUpdated={setUpdated} currentUser={currentUser} users={users} updateUserAdd={updateUserAdd} />
+				</Modal.Body>
+				<Modal.Footer>
+					<Button variant="secondary" onClick={handleClose}>
+						Close
+					</Button>
+				</Modal.Footer>
+			</Modal>
+				{allResults}
+			</div>
+		</section>
+	)
 };
 
 export default FriendsPage;
