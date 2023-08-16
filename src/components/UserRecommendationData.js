949b2e736ea2f482eefccfd8a19b8304aa1e437a
liau11 <aulily09@gmail.com>
2023-08-16 13:41:35 -0700

    Styling, positioning, centering

--- src/components/UserRecommendationData.js
+++ src/components/UserRecommendationData.js
@@ -5,6 +5,7 @@ import './CardStyle.css';
 import { Icon } from '@iconify/react';
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import Tooltip from 'react-bootstrap/Tooltip';
+// import '../pages/ProfilePage.css'; 
 
 
 const UserRecommendationData = ({ updateUserDelete, restaurantId, name, address, url, price, imageUrl }) => {
@@ -18,42 +19,44 @@ const UserRecommendationData = ({ updateUserDelete, restaurantId, name, address,
 	};
 
 	return (
-		<Card style={{ width: '18rem', marginTop: '40px' , marginBottom: '40px'}} className='border p-4' id='card'>
-			<div className='card-image-container'>
-				<Card.Img variant='top' src={imageUrl} className='centered-image' />
-			</div>
-			<Card.Body>
-				<Card.Title>{name}</Card.Title>
-				<Card.Text>{address}</Card.Text>
-				<Card.Text>{price}</Card.Text>
-				<Card.Footer  className='d-flex justify-content-center align-items-center'>
-				<OverlayTrigger
-					placement='top'
-					overlay={<Tooltip id={'yelp-msg'} style={{position:"fixed"}}>Open Yelp</Tooltip>}
-				>
-					<Button className="btn-sm" variant="outline-danger" onClick={openYelpInNewTab}>
-						<Icon icon="bi:yelp" color="red" width="18" height="18" />
-					</Button>
-				</OverlayTrigger>
-				<OverlayTrigger
-					placement='top'
-					overlay={<Tooltip id={'google-maps-msg'} style={{position:"fixed"}}>Open Google Maps</Tooltip>}
-				>
-					<Button className="btn-sm" variant="info" onClick={openGoogleMaps}>
-						<Icon icon="bi:map" color="white" width="18" height="18" />
-					</Button>
-				</OverlayTrigger>
-				<OverlayTrigger
-					placement='top'
-					overlay={<Tooltip id={'delete-msg'} style={{position:"fixed"}}>Delete Recommendation</Tooltip>}
-				>
-					<Button className='btn-sm' variant='danger' onClick={() => updateUserDelete('recommendations', {'recommendations': restaurantId })}>
-						<Icon icon="bi:trash" width="18" height="18" />
-					</Button>
-				</OverlayTrigger>
-				</Card.Footer>
-			</Card.Body>
-		</Card>
+		<div className='grid-item'>
+			<Card style={{ width: '18rem', marginTop: '40px' , marginBottom: '40px'}} className='border p-4' id='card'>
+				<div className='card-image-container'>
+					<Card.Img variant='top' src={imageUrl} className='centered-image' />
+				</div>
+				<Card.Body>
+					<Card.Title>{name}</Card.Title>
+					<Card.Text>{address}</Card.Text>
+					<Card.Text>{price}</Card.Text>
+					<Card.Footer  className='d-flex justify-content-center align-items-center'>
+					<OverlayTrigger
+						placement='top'
+						overlay={<Tooltip id={'yelp-msg'} style={{position:"fixed"}}>Open Yelp</Tooltip>}
+					>
+						<Button className="btn-sm" variant="outline-danger" onClick={openYelpInNewTab}>
+							<Icon icon="bi:yelp" color="red" width="18" height="18" />
+						</Button>
+					</OverlayTrigger>
+					<OverlayTrigger
+						placement='top'
+						overlay={<Tooltip id={'google-maps-msg'} style={{position:"fixed"}}>Open Google Maps</Tooltip>}
+					>
+						<Button className="btn-sm" variant="info" onClick={openGoogleMaps}>
+							<Icon icon="bi:map" color="white" width="18" height="18" />
+						</Button>
+					</OverlayTrigger>
+					<OverlayTrigger
+						placement='top'
+						overlay={<Tooltip id={'delete-msg'} style={{position:"fixed"}}>Delete Recommendation</Tooltip>}
+					>
+						<Button className='btn-sm' variant='danger' onClick={() => updateUserDelete('recommendations', {'recommendations': restaurantId })}>
+							<Icon icon="bi:trash" width="18" height="18" />
+						</Button>
+					</OverlayTrigger>
+					</Card.Footer>
+				</Card.Body>
+			</Card>
+		</div>
 	);
 };
 
