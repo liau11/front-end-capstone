bebd944ea94aedbfcceb7d65bf3762e2ced1e90c
Sophia Tran <sophiat8832@gmail.com>
2023-08-16 10:21:49 -0700

    Style button

--- src/components/RestaurantForm.js
+++ src/components/RestaurantForm.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import PropTypes from "prop-types";
 import { Card, Button, Col, Row, Form } from 'react-bootstrap';
 import './CardStyle.css';
-import './CardStyle.css';
+import './GetRecommendationsForm.css';
 
 const RestaurantForm = ({ currentUser, handleAddToList, allRestaurants, addNewRestaurant, updateUserAdd }) => {
 
@@ -79,7 +79,7 @@ const RestaurantForm = ({ currentUser, handleAddToList, allRestaurants, addNewRe
                         />
                     </Col>
                     <Col sm="1" className="mt-2">
-                        <Button type="submit" variant="outline-primary" size="sm" className="ms-1">
+                        <Button type="submit" id="find-restaurants-button" variant="outline-primary" size="sm" className="ms-1">
                             Submit
                         </Button>
                     </Col>
